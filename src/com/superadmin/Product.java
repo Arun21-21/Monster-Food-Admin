b66176937348b3223384024ecbe68b6f270ec2db
Arun21-21 <arunpandian@cloudinlabs.com>
2023-11-06 18:17:23 +0530

    working on feed module

--- src/com/superadmin/Product.java
+++ src/com/superadmin/Product.java
@@ -22,7 +22,7 @@ public class Product {
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		d.get("https://test-monster-chicken-admin.vercel.app/auth/login");
-		Thread.sleep(200);
+		Thread.sleep(200);		
 		
 // invalid login
 		
@@ -38,7 +38,7 @@ public class Product {
 				.click();
 		Thread.sleep(5000);
 		d.navigate().refresh();
-		// valid login
+// valid login
 		d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[1]/input"))
 				.sendKeys("superAdmin@monster.chicken");
 		Thread.sleep(1000);
@@ -217,7 +217,7 @@ public class Product {
 				.sendKeys("Raw product | Chicken");
 		Thread.sleep(500);
 		d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[12]/div/input"))
-				.sendKeys("Raw product | Chicken)");
+				.sendKeys("Raw product | Chicken");
 		Thread.sleep(500);
 		d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[13]/div/textarea"))
 				.sendKeys("Raw product | Chicken");
@@ -226,6 +226,7 @@ public class Product {
 		Thread.sleep(500);
 		d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/button")).click();
 		d.navigate().refresh();
+		Thread.sleep(2000);
 
 // Product filter
 		
@@ -352,7 +353,7 @@ public class Product {
 		h.keyPress(KeyEvent.VK_ENTER);
 		d.findElement(By.xpath(
 				"//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-add/div/div[2]/div/form/div/div/div/div[2]/div/input"))
-				.sendKeys("Best Quanlity");
+				.sendKeys("Best Quality");
 		Thread.sleep(3000);
 		d.findElement(By.xpath("//*[@id=\"btn-save-event\"]")).click();
 		Thread.sleep(3000);
@@ -422,7 +423,6 @@ Thread.sleep(1000);
 		d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
 		Thread.sleep(2000);
 	
-//super admin successfully assigned the supplier 
 		
 //Cutting center ready to receive the order 
 		
@@ -443,7 +443,40 @@ Thread.sleep(1000);
 	    Thread.sleep(1000);
 	    d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
 	    Thread.sleep(1000);
-//Make payment 
+	    
+// Mortality 
+	   
+	    d.get("https://test-monster-chicken-admin.vercel.app/mortality");
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]/a/i")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[4]/ul/li[5]/a")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-mortality-list/div/div/div[1]/div/div/div[2]/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[1]/div/ng-select/div/div/div[2]")).click();
+	    Thread.sleep(1000);
+	    Robot mortality = new Robot();
+	    mortality.keyPress(KeyEvent.VK_DOWN);
+	    mortality.keyPress(KeyEvent.VK_DOWN);
+	    mortality.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[2]/div/ng-select/div/div/div[2]")).click();
+	    Thread.sleep(1000);
+	    Robot mortalityproduct = new Robot();
+	    mortalityproduct.keyPress(KeyEvent.VK_DOWN);
+	    mortalityproduct.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[3]/div/input")).sendKeys("10");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"btn-save-event\"]")).click();	
+	    Thread.sleep(1000);
+
+	  	    
+//Make payment
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[5]/a/i")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div/ngx-simplebar/div[1]/div[2]/div/div/div/div/div/ul/li[5]/ul/li[1]/a")).click();
+	    Thread.sleep(1000);
 	    d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/app-purchase-order-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[6]/div[1]/button")).click();
 	    Thread.sleep(1000);
 	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[1]/div/ng-select/div/span")).click();
@@ -454,12 +487,272 @@ Thread.sleep(1000);
 	    payment.keyPress(KeyEvent.VK_ENTER);
 	    Thread.sleep(200);
 	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[2]/button")).click();
-	    Thread.sleep(1000);
+	    Thread.sleep(3000);
+	      
 // search by purchase number in CC
 	 // d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-list/div/div[1]/div[2]/div/div[2]/div[2]/div/input")).click();
 	  //d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-list/div/div[1]/div[2]/div/div[2]/div[2]/div/input")).sendKeys("MC-PO-47");
 	   // Thread.sleep(2000);
 	   // d.navigate().refresh();
+	   
+//Walk-in customer 
+	    
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[3]/a/i")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div/ngx-simplebar/div[1]/div[2]/div/div/div/div/div/ul/li[3]/ul/li[5]/a")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[2]/div/div[1]/div[2]/input")).click();
+	    Thread.sleep(500);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[4]/div/div/input")).click();
+	    Thread.sleep(2000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[2]/div/div[1]/div[2]/input")).sendKeys("saran");
+	    Thread.sleep(2000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[2]/div/div[2]/div[2]/input")).sendKeys("9871235678");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[3]/div/div/div[2]/div[2]/div/div/div/div[2]/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    Robot walk = new Robot();
+	    walk.keyPress(KeyEvent.VK_DOWN);
+	    walk.keyRelease(KeyEvent.VK_DOWN);
+	    walk.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[3]/div/div/div[2]/div[2]/div/div/div/div[3]/input")).sendKeys("10");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[3]/div/div/div[1]/button/i")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[3]/div/div/div[2]/div[2]/div[2]/div/div/div[2]/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    walk.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[3]/div/div/div[2]/div[2]/div[2]/div/div/div[3]/input")).sendKeys("10");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-walk-in-customer/div/form/div[4]/div/div/input")).click();
+	    Thread.sleep(3000);
+	   
+	    
+// Walk in customer order reflects in order list in the name of walk in customer 
+	     
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[3]/ul/li[1]/a")).click();
+	    
+//Order list filter by order type 
+	    
+	    Thread.sleep(2000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-order-list/div/div[1]/div[2]/div/div[2]/div[1]/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    Robot online = new Robot();
+	    online.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(3000);
+	    d.navigate().refresh();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-order-list/div/div[1]/div[2]/div/div[2]/div[1]/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    Robot order = new Robot();
+	    order.keyPress(KeyEvent.VK_DOWN);
+	    order.keyRelease(KeyEvent.VK_DOWN);
+	    order.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(3000);
+	    d.navigate().refresh();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-order-list/div/div[1]/div[2]/div/div[2]/div[1]/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    Robot inline = new Robot();
+	    inline.keyPress(KeyEvent.VK_DOWN);
+	    inline.keyRelease(KeyEvent.VK_DOWN);
+	    inline.keyPress(KeyEvent.VK_DOWN);
+	    inline.keyRelease(KeyEvent.VK_DOWN);
+	    inline.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(3000);
+	    
+// Feed purchase oder
+	    
+	    d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[7]/a/i")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div/ngx-simplebar/div[1]/div[2]/div/div/div/div/div/ul/li[7]/ul/li[1]/a")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[1]/div[1]/div/div/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-add/div/div[2]/div/form/div/div/div/div[2]/div/input")).click();
+	    Thread.sleep(500);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-add/div/div[2]/div/form/div/div/div/div[2]/div/input")).sendKeys("Best Quality");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/div/div/app-purchase-order-add/div/div[2]/div/form/div/div/div/div[3]/div/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-edit/div/div[3]/div/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[1]/div/ng-select/div/span")).click();
+	    Thread.sleep(1000);
+	    Robot feedpurchase = new Robot();
+	    feedpurchase.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[2]/div/input")).sendKeys("10");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[3]/div/input")).sendKeys("Best Quality");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[4]/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-purchase-order-edit/div/div[1]/div/div/div/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[5]/div/button")).click();
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
+	    Thread.sleep(2000);
+	    
+// super admin assign supplier to feed purchase 
+	    
+	    d.get("https://test-monster-chicken-admin.vercel.app/auth/login");
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[1]/input"))
+		.sendKeys("superAdmin@monster.chicken");
+Thread.sleep(1000);
+d.findElement(
+		By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[2]/div/input"))
+		.sendKeys("Monster@23Chicken");
+Thread.sleep(1000);
+d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[3]/button"))
+		.click();
+Thread.sleep(2000);
+          d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[8]/a/i")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div/ngx-simplebar/div[1]/div[2]/div/div/div/div/div/ul/li[8]/ul/li[1]/a/span")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[7]/div/button")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[7]/div/button")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[1]/div/ng-select/div/span")).click();
+          Thread.sleep(1000);
+          Robot feedsupplier = new Robot();
+          feedsupplier.keyPress(KeyEvent.VK_ENTER);
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[2]/button")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[7]/div/button")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[7]/div/button")).click();
+          Thread.sleep(1000);
+          d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
+          Thread.sleep(3000);
+
+// cutting center receives the order and completed the payment 
+          
+          d.get("https://test-monster-chicken-admin.vercel.app/auth/login");
+          Thread.sleep(1000);
+          d.findElement(By.xpath("//*[@id=\"userName\"]")).click();
+  		Thread.sleep(3000);
+  		d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[1]/input"))
+  				.sendKeys("jhzpoenmuzqmajkrfv@cazlg.com");
+  		Thread.sleep(3000);
+  		d.findElement(
+  				By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[2]/div/input"))
+  				.sendKeys("123monsterchicken");
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[3]/button"))
+  				.click();
+  		Thread.sleep(2000);
+  		d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[7]/a/i")).click();
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[7]/ul/li[1]/a/span")).click();
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[5]/div/button")).click();
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("/html/body/div/div/div[6]/button[1]")).click();
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-feed-list/div/div[2]/div/div/div/div/table/tbody/tr[1]/td[5]/div[1]/button")).click();
+  		Thread.sleep(1000);
+  		d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[1]/div/ng-select/div/span")).click();
+  		Thread.sleep(1000);
+  		Robot feedpayment = new Robot();
+	    feedpayment.keyPress(KeyEvent.VK_ENTER);
+	    Thread.sleep(1000);
+	    d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/form/div/div[2]/button")).click();
+	    Thread.sleep(3000);
+	    
+// Adding supplier
+	    
+		d.get("https://test-monster-chicken-admin.vercel.app/auth/login");
+		Thread.sleep(200);
+		d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[1]/input"))
+		.sendKeys("superAdmin@monster.chicken");
+Thread.sleep(1000);
+d.findElement(
+		By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[2]/div/input"))
+		.sendKeys("Monster@23Chicken");
+Thread.sleep(1000);
+d.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/div[1]/div[2]/div[2]/form/div[3]/button"))
+		.click();
+Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"side-menu\"]/li[6]/a/i")).click();
+      Thread.sleep(1000);
+      d.findElement(By.xpath("/html/body/app-root/app-layout/app-vertical/div/app-sidebar/div/ngx-simplebar/div[1]/div[2]/div/div/div/div/div/ul/li[6]/ul/li[4]/a")).click();
+      Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendors-list/div/div[1]/div[1]/div/div/button")).click();
+      Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[1]/div/input")).sendKeys("Egg supplier");
+      Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[2]/div/input")).sendKeys("eggsupplier@gmail.com");
+      Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[3]/div/input")).sendKeys("9876653456");
+      Thread.sleep(1000);
+      d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[4]/div/ng-select")).click();
+      Thread.sleep(1000);
+      Robot addsupplier = new Robot();
+      addsupplier.keyPress(KeyEvent.VK_DOWN);
+	  addsupplier.keyRelease(KeyEvent.VK_DOWN);
+	  addsupplier.keyPress(KeyEvent.VK_DOWN);
+	  addsupplier.keyRelease(KeyEvent.VK_DOWN);
+	  addsupplier.keyPress(KeyEvent.VK_ENTER);
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[5]/div/ng-select")).click();
+	  Robot gender = new Robot();
+	  gender.keyPress(KeyEvent.VK_ENTER);
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[6]/div/input")).sendKeys("33AABCU9603R1ZU");
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[7]/div/input")).sendKeys("ABCPS1234A");
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"btn-save-event\"]")).click();
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("/html/body/ngb-modal-window/div/div/div[2]/app-supplier-location/div/div[2]/button[2]")).click();
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[10]/div/input")).sendKeys("Near to bus stand");
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[11]/div/ng-select")).click();
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[11]/div/ng-select")).click();
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[11]/div/ng-select")).sendKeys("Tamil nadu");
+	  Thread.sleep(1000);
+	  d.findElement(By.xpath("//*[@id=\"layout-wrapper\"]/div/div/app-vendor-form/div/div[2]/div/form/div/div[11]/div/ng-select")).click();
+	  
+	  
+	  
+     
+	    
+
+	    
+	    
+  		
+  		
+          
+          
+          
+          
+
+	    
+          
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	    
 	    
 	    
